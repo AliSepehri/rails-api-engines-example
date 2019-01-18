28d4bbde3602e448efdbd8f8d7173d3074dff070
Ali Sepehri.Kh <ali.sepehri.kh@gmail.com>
2019-01-18 21:16:40 +0330

    Set migration folder's path to `models/db/migrate`

--- core/config/application.rb
+++ core/config/application.rb
@@ -31,5 +31,7 @@ module RailsApiEnginesExample
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.paths['db/migrate'] = Models::Engine.paths['db/migrate'].existent
   end
 end
